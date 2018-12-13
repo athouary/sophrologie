bf85c554bad179e2bf1580c1399398f762cdda5b
Auxane Thouary <athouary@gmail.com>
2018-12-13 18:35:33 +0100

    Remove extra folder

--- sophrologie/wp-content/themes/simple-business-wp/parts/wp_link_pages.php
+++
@@ -1,9 +0,0 @@
-<?php
-$args = array(
-    'before'           => '<p class="wp_link_pages">' . __('Pages:', 'simple-business-wp'),
-    'after'            => '</p>',
-    'link_before'      => '<span>',
-    'link_after'       => '</span>'
-);
-wp_link_pages($args);
-?>
