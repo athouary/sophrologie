68b5cfa142b45947aa55beff4c74a1284572517a
Auxane Thouary <athouary@parsys.com>
2018-12-22 10:25:55 +0100

    add text on banner

--- wp-content/themes/simple-business-wp/parts/frontpage-banner.php
+++ wp-content/themes/simple-business-wp/parts/frontpage-banner.php
@@ -2,6 +2,9 @@
     <?php if (simple_business_wp_get_option('fp-banner-toggle') == "1") { ?>
         <div>
             <img id="frontpage_banner" class="static_banner nimbus_1168_526" src="<?php echo esc_url(simple_business_wp_get_option('simple_business_wp_content_width_banner')); ?>" />
+            <div class="frontpage_banner_text_wrapper">
+                <h1 class="frontpage_banner_text">Sophrologie</h1>
+            </div>
         </div>
     <?php } else if (simple_business_wp_get_option('fp-banner-toggle') == "3") { ?>
             <h2 style="text-align:center;font-weight:bold;"><?php _e("Please update to the <a style='font-weight:bold;color:blue;' href='http://www.nimbusthemes.com/wordpress-themes/simple-business/'>Pro version</a> of this theme in order to use this slideshow feature.", "simple-business-wp"); ?></h2>
