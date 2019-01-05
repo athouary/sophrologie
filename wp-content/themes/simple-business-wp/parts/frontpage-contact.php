39771a9e9a16ad12a77dbc2790fb6ae9740ded73
Auxane Thouary <athouary@parsys.com>
2019-01-05 15:24:54 +0100

    feat(contact): rename fields

--- wp-content/themes/simple-business-wp/parts/frontpage-contact.php
+++ wp-content/themes/simple-business-wp/parts/frontpage-contact.php
@@ -9,14 +9,14 @@
                     <?php if (simple_business_wp_get_option('fp-contact-sub-title') != '') { ?>
                         <div class="contact-sub-title h4"><?php echo esc_html(simple_business_wp_get_option('fp-contact-sub-title')); ?></div>
                     <?php } ?>
-                    <?php 
-                    if(isset($_POST['submitted'])) { 
+                    <?php
+                    if(isset($_POST['submitted'])) {
                         if(isset($_POST['myname']) && isset($_POST['email'])) {
                             if((trim($_POST['myname']) != "" ) && (trim($_POST['email']) != "" )) { ?>
                                 <p class="bg-success"><?php echo _e('Thanks for contacting us!', 'simple-business-wp'); ?></p>
                             <?php } else { ?>
                                 <p class="bg-danger"><?php echo _e('Please enter your name and email address.', 'simple-business-wp'); ?></p>
-                            <?php 
+                            <?php
                             }
                         }
                     }
@@ -26,13 +26,13 @@
                         if($simple_business_wp_contact_recipient_email != '' && $simple_business_wp_contact_sender_email != '') {
                             extract($_POST);
                             $blog_name = get_bloginfo('name');
-                            $message = "Name: $myname
+                            $message = "Nom: $myname
                                         Email: $email
-                                        Website: $website
-                                        Comments: $comments
+                                        Objet: $website
+                                        Message: $comments
                             ";
                             $headers = 'From: '.$blog_name.' <'.$simple_business_wp_contact_sender_email.'>' . "\r\n";
-                            wp_mail($simple_business_wp_contact_recipient_email, 'Contact Form', $message, $headers);
+                            wp_mail($simple_business_wp_contact_recipient_email, 'Formulaire de contact', $message, $headers);
                         }
                     }
                     ?>
@@ -41,12 +41,12 @@
                              <form class="contact-form" name="contact-form" method="post">
                                 <div class="row">
                                     <div class="col-sm-4">
-                                        <div class="form-group"><input data-sr="wait 0.3s, enter left and move 50px after 1s" type="text" id="myname" name="myname" placeholder="Name*" class="form-control input-lg" /></div>
+                                        <div class="form-group"><input data-sr="wait 0.3s, enter left and move 50px after 1s" type="text" id="myname" name="myname" placeholder="Nom*" class="form-control input-lg" /></div>
                                         <div class="form-group"><input data-sr="wait 0.3s, enter left and move 50px after 1s" type="text" id="email" name="email" placeholder="Email*" class="form-control input-lg" /></div>
-                                        <div class="form-group"><input data-sr="wait 0.3s, enter left and move 50px after 1s" type="text" id="website" name="website" placeholder="Website" class="form-control input-lg" /></div>
+                                        <div class="form-group"><input data-sr="wait 0.3s, enter left and move 50px after 1s" type="text" id="website" name="website" placeholder="Objet" class="form-control input-lg" /></div>
                                     </div>
                                     <div class="col-sm-8">
-                                        <textarea data-sr="wait 0.3s, enter right and move 50px after 1s" id="comments" name="comments" class="form-control input-lg" rows="6" cols="" placeholder="Comments"></textarea>
+                                        <textarea data-sr="wait 0.3s, enter right and move 50px after 1s" id="comments" name="comments" class="form-control input-lg" rows="6" cols="" placeholder="Message"></textarea>
                                     </div>
                                 </div>
                                 <br />
@@ -58,27 +58,27 @@
                             </form>
                         </div>
                     </div>
-                </div> 
-            </div>    
-        </div>    
-    </section> 
+                </div>
+            </div>
+        </div>
+    </section>
 <?php } else if (simple_business_wp_get_option('contact-toggle') == '3') {
     // Don't do anything
-} else { ?>  
+} else { ?>
     <section id="<?php if (simple_business_wp_get_option('fp-contact-slug')=='') {echo "contact";} else {echo esc_attr(simple_business_wp_get_option('fp-contact-slug'));} ?>" class="frontpage-contact">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
                     <div class="contact-title h1">Contact Us</div>
                     <div class="contact-sub-title h4">Let us know what you are thinking.</div>
-                    <?php 
-                    if(isset($_POST['submitted'])) { 
+                    <?php
+                    if(isset($_POST['submitted'])) {
                         if(isset($_POST['myname']) && isset($_POST['email'])) {
                             if((trim($_POST['myname']) != "" ) && (trim($_POST['email']) != "" )) { ?>
                                 <p class="bg-success"><?php echo _e('Thanks for contacting us!', 'simple-business-wp'); ?></p>
                             <?php } else { ?>
                                 <p class="bg-danger"><?php echo _e('Please enter your name and email address.', 'simple-business-wp'); ?></p>
-                            <?php 
+                            <?php
                             }
                         }
                     }
@@ -108,8 +108,8 @@
                             </form>
                         </div>
                     </div>
-                </div> 
-            </div>    
-        </div>    
-    </section> 
-<?php } ?> 
+                </div>
+            </div>
+        </div>
+    </section>
+<?php } ?>
