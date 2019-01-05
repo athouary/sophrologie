c5058661900b7109794ffd18f767e352751ba7a8
Auxane Thouary <athouary@parsys.com>
2019-01-05 14:41:50 +0100

    feat(team): put hard image

--- wp-content/themes/simple-business-wp/parts/frontpage-team.php
+++ wp-content/themes/simple-business-wp/parts/frontpage-team.php
@@ -20,7 +20,7 @@
                     <div class="team-sub-title h4"><?php echo simple_business_wp_get_option('fp-team-sub-title'); ?></div>
                     <div class="centered">
                         <div class="team-item" data-sr="wait 0.3s, enter right and move 50px after 1s">
-                            <img class="img-responsive img-circle center-block" src="<?php echo esc_url(simple_business_wp_get_option('fp-team-image')); ?>" />
+                            <img class="img-responsive img-circle center-block" src="<?php echo get_home_url(); ?>/wp-content/uploads/2019/01/Capture-d’écran-2019-01-05-à-14.20.06-e1546694497675-768x768.png" />
                             <h4 class="team-item-title"><?php echo simple_business_wp_get_option('fp-team-name'); ?></h4>
                             <h5 class="team-item-sub-title"><?php echo simple_business_wp_get_option('fp-team-job'); ?></h5>
                         </div>
