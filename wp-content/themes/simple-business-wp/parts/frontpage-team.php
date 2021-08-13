bb2a4a5f63352733053eaaa59ad2282a4443fb51
Auxane Thouary <athouary@parsys.com>
2021-08-13 12:27:26 +0200

    change portrait image

--- wp-content/themes/simple-business-wp/parts/frontpage-team.php
+++ wp-content/themes/simple-business-wp/parts/frontpage-team.php
@@ -20,7 +20,7 @@
                     <div class="team-sub-title h4"><?php echo simple_business_wp_get_option('fp-team-sub-title'); ?></div>
                     <div class="centered">
                         <div class="team-item" data-sr="wait 0.3s, enter right and move 50px after 1s">
-                            <img class="img-responsive img-circle center-block" src="<?php echo get_home_url(); ?>/wp-content/uploads/2019/01/Nath2-1-e1546696447730-300x300.jpg" alt="Nathalie Thouary Solon Sophrologue certifiée" />
+                            <img class="img-responsive img-circle center-block" src="<?php echo get_home_url(); ?>/wp-content/uploads/2021/08/Portrait.jpeg" alt="Nathalie Thouary Solon Sophrologue certifiée" />
                             <h4 class="team-item-title"><?php echo simple_business_wp_get_option('fp-team-name'); ?></h4>
                             <h5 class="team-item-sub-title"><?php echo simple_business_wp_get_option('fp-team-job'); ?></h5>
                             <div class="text-center team-button">
