51b6f5808647bf49e78a8a0e87b58c01a4c84266
Auxane <athouary@parsys.com>
2023-02-25 13:59:34 +0100

    invert thouary and solon

--- wp-content/themes/simple-business-wp/parts/frontpage-team.php
+++ wp-content/themes/simple-business-wp/parts/frontpage-team.php
@@ -20,7 +20,7 @@
                     <div class="team-sub-title h4"><?php echo simple_business_wp_get_option('fp-team-sub-title'); ?></div>
                     <div class="centered">
                         <div class="team-item" data-sr="wait 0.3s, enter right and move 50px after 1s">
-                            <img class="img-responsive img-circle center-block" src="<?php echo get_home_url(); ?>/wp-content/uploads/2021/08/Portrait.jpeg" alt="Nathalie Thouary Solon Sophrologue certifiée" />
+                            <img class="img-responsive img-circle center-block" src="<?php echo get_home_url(); ?>/wp-content/uploads/2021/08/Portrait.jpeg" alt="Nathalie Solon Thouary Sophrologue certifiée" />
                             <h4 class="team-item-title"><?php echo simple_business_wp_get_option('fp-team-name'); ?></h4>
                             <h5 class="team-item-sub-title"><?php echo simple_business_wp_get_option('fp-team-job'); ?></h5>
                             <div class="text-center team-button">
@@ -31,9 +31,9 @@
                 </div>
             </div>
             <div class="row link-image-row">
-                <a target="_blank" href="http://www.rncp.cncp.gouv.fr/grand-public/visualisationFiche?format=fr&fiche=13375"><img class="img-responsive center-block link-image" src="<?php echo get_home_url(); ?>/wp-content/uploads/2019/01/cncp-e1546697437540-300x235.jpg" alt="Comission nationale de la certification professionnelle Nathalie Thouary Solon" /></a>
-                <a target="_blank" href="https://www.sophrologie-formation.fr"><img class="img-responsive center-block link-image" src="<?php echo get_home_url(); ?>/wp-content/uploads/2019/01/aliotta-300x300.jpg" alt="Aliotta formations Nathalie Thouary Solon" /></a>
-                <a target="_blank" href="https://www.chambre-syndicale-sophrologie.fr/titre-rncp/"><img class="img-responsive center-block link-image" src="<?php echo get_home_url(); ?>/wp-content/uploads/2019/01/css-300x295.png" alt="Chambre syndicale de la sophrologie Nathalie Thouary Solon"/></a>
+                <a target="_blank" href="http://www.rncp.cncp.gouv.fr/grand-public/visualisationFiche?format=fr&fiche=13375"><img class="img-responsive center-block link-image" src="<?php echo get_home_url(); ?>/wp-content/uploads/2019/01/cncp-e1546697437540-300x235.jpg" alt="Commission nationale de la certification professionnelle Nathalie Solon Thouary" /></a>
+                <a target="_blank" href="https://www.sophrologie-formation.fr"><img class="img-responsive center-block link-image" src="<?php echo get_home_url(); ?>/wp-content/uploads/2019/01/aliotta-300x300.jpg" alt="Aliotta formations Nathalie Solon Thouary" /></a>
+                <a target="_blank" href="https://www.chambre-syndicale-sophrologie.fr/titre-rncp/"><img class="img-responsive center-block link-image" src="<?php echo get_home_url(); ?>/wp-content/uploads/2019/01/css-300x295.png" alt="Chambre syndicale de la sophrologie Nathalie Solon Thouary"/></a>
             </div>
         </div>
     </section>
